ec1bc5dadac70cb4cd6f47b72184e549e2fb1b87
Jonathan William <Jjoninha11@hotmail.com>
2024-04-25 10:16:45 -0300

    Projeto iniciado

---
+++ src/test/java/com/servicopedidos/ServicoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicopedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
